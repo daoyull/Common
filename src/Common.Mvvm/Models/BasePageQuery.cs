599735850f8f189cd92063856290cc55b6afe69d
DaoYu <daoyu5313@163.com>
2024-08-02 14:53:54 +0800

    Add Mvvm And PageModel

---
+++ src/Common.Mvvm/Models/BasePageQuery.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Newtonsoft.Json;
+
+namespace Common.Mvvm.Models;
+
+public partial class BasePageQuery : ObservableObject
+{
+    /// <summary>
+    /// 页数
+    /// </summary>
+    [JsonProperty("pageNum")] [ObservableProperty]
+    private int _pageNum = 1;
+
+    /// <summary>
+    /// 页码
+    /// </summary>
+    [JsonProperty("pageSize")] [ObservableProperty]
+    private int _pageSize = 10;
+}
---
+++ src/Common.Mvvm/Models/PageResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Mvvm.Models;
+
+public class PageResult<T>
+{
+    public int Total { get; set; }
+
+    public List<T> List { get; set; }
+
+    public PageResult(int total, List<T> list)
+    {
+        Total = total;
+        List = list;
+    }
+}
