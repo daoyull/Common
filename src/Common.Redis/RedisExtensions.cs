6f2c90bc9f3e9e6328c12abb0cffdc3407cd39db
DaoYu <daoyu5313@163.com>
2024-08-20 17:37:46 +0800

    FreeSql Redis Change Result

--- src/Common.Redis/RedisExtensions.cs
+++ src/Common.Redis/RedisExtensions.cs
@@ -1,20 +1,27 @@
 ﻿using System.Diagnostics;
 using Autofac;
+using Common.Lib.Exceptions;
+using LanguageExt.Common;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using StackExchange.Redis;
 
 namespace Common.Redis;
 
-public delegate IDatabase RedisResolver(string key, int database = 0);
+public delegate Result<IDatabase> RedisResolver(string key, int database = 0);
 
 public static class RedisExtensions
 {
-    
-
     private static readonly Dictionary<string, IConnectionMultiplexer> RedisDictionary = new();
 
     private static readonly object Locker = new object();
+
+    public static ContainerBuilder AddEmptyRedis(this ContainerBuilder container)
+    {
+        var redisResolver = new RedisResolver((_, _) => new Result<IDatabase>(new BusinessException("Empty Redis")));
+        container.RegisterInstance(redisResolver).SingleInstance();
+        return container;
+    }
 
     public static ContainerBuilder AddRedis(this ContainerBuilder container)
     {
@@ -25,14 +32,14 @@ public static class RedisExtensions
                 lock (Locker)
                 {
                     if (RedisDictionary.TryGetValue(key, out var multiplexer))
-                        return multiplexer.GetDatabase(database);
+                        return new Result<IDatabase>(multiplexer.GetDatabase(database));
 
                     // 后来注册的情况
                     var optionsSnapshot = provider.ResolveOptional<IOptionsSnapshot<RedisOptions>>();
                     var redisOption = optionsSnapshot?.Get(key);
 
-                    if (redisOption == null)
-                        throw new KeyNotFoundException($"未添加{key}的Redis连接配置");
+                    if (redisOption == null || string.IsNullOrEmpty(redisOption.Ip))
+                        return new Result<IDatabase>(new KeyNotFoundException($"未添加{key}的Redis连接配置"));
                     var connectStr = redisOption.GenConnectStr();
                     var logger = provider.Resolve<ILogger<IConnectionMultiplexer>>();
 
@@ -42,7 +49,7 @@ public static class RedisExtensions
 
                     RedisDictionary.Add(key, multipartContent);
 
-                    return multipartContent.GetDatabase(database);
+                    return new Result<IDatabase>(multipartContent.GetDatabase(database));
                 }
             });
         }).SingleInstance();
